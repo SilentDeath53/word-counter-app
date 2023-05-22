888d3a80526910239c678fffeb678b33ff793817
SilentDeath53 <48599339+SilentDeath53@users.noreply.github.com>
2023-05-22 18:12:24 +0300

    Update main.java

--- main.java
+++ main.java
@@ -7,27 +7,22 @@ import java.util.Scanner;
 public class WordCounter {
 
     public static void main(String[] args) {
-        // Kullanıcıdan metin belgesini seçmesini isteyin
         String filePath = getFilePathFromUser();
         
-        // Metin belgesini okuyun
         String text = readTextFromFile(filePath);
         
-        // Kelimeleri say ve frekansını hesapla
         Map<String, Integer> wordCounts = countWords(text);
         
-        // Sonuçları ekrana yazdır
         printWordCounts(wordCounts);
     }
     
     public static String getFilePathFromUser() {
-        // Kullanıcıdan dosya seçmesini isteyin ve dosya yolunu döndürün
-        // Bu kısmı arayüzünüze veya konsol girişine uygun şekilde düzenleyebilirsiniz
+        // Ask user to select file and return file path
+        // You can edit this part to suit your interface or console input
     }
     
     public static String readTextFromFile(String filePath) {
         try {
-            // Dosyayı okuyun ve içeriği bir String'e aktarın
             File file = new File(filePath);
             Scanner scanner = new Scanner(file);
             
@@ -48,7 +43,6 @@ public class WordCounter {
     public static Map<String, Integer> countWords(String text) {
         Map<String, Integer> wordCounts = new HashMap<>();
         
-        // Metni kelimelere ayır ve kelime sayısını hesapla
         String[] words = text.split("\\s+");
         for (String word : words) {
             wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
@@ -58,7 +52,6 @@ public class WordCounter {
     }
     
     public static void printWordCounts(Map<String, Integer> wordCounts) {
-        // Kelime sayısını ve frekansını ekrana yazdır
         for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
             String word = entry.getKey();
             int count = entry.getValue();
